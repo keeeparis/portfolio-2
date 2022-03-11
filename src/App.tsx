42845b79c1e6ce3714c239841e98f558b83c0167
Keeeparis <vladimir1trotsenko@gmail.com>
2022-03-11 16:48:50 +0600

    main logic in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,39 @@
-import styled from 'styled-components'
-import Header from './container/Header/Header'
-import Sort from './container/Sort/Sort'
-import Main from './container/Main/Main'
+import Header from './container/Header'
+import Sort from './container/Sort'
+import Main from './container/Main'
+import rawData from './mock/index.json'
+
 import { InnerWrapper, Wrapper } from './styles'
-
-
+import { ChangeEvent, useState } from 'react'
+import { useItems } from './hooks/useItems'
 
 function App() {
+	const [categorySort, setCategorySort] = useState('isAll')
+	const [selectedOption, setSelectedOption] = useState('популярности')
+
+	const sortedItems = useItems(rawData, categorySort, selectedOption)
+
+    const handleInputRadioChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setCategorySort(e.target.value)
+    }
+
+	const handleSortingChange = (value: string) => {
+        setSelectedOption(value)
+    }
+
 	return (
 		<Wrapper>
 			<InnerWrapper>
 				<Header />
-				<Sort />
-				<Main />
+				<Sort 
+					handleInputRadioChange={handleInputRadioChange} 
+					handleSortingChange={handleSortingChange}
+					categorySort={categorySort} 
+					selectedOption={selectedOption}
+				/>
+				<Main 
+					items={sortedItems} 
+				/>
 			</InnerWrapper>
 		</Wrapper>
 	)
