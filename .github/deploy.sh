f2d1c4549a44cff82d10e593899c617e871f217b
Keeeparis <vladimir1trotsenko@gmail.com>
2022-03-14 18:47:20 +0600

    check it works

--- .github/deploy.sh
+++ .github/deploy.sh
@@ -17,11 +17,9 @@ git init
 git checkout -b main
 git add -A
 
-git config --local user.email "vladimir1trotsenko@gmail.com"
-git config --local user.name "Keeeparis"
+git config --local user.email "githubActions"
+git config --local user.name "byKeeeparis"
 
 git commit -m 'deploy'
-
-# git push -f git@github.com:keeeparis/portfolio-2.git main:gh-pages
 
 cd -
