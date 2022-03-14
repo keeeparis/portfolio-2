4bbe962c6dc56e42ad17da3f27f852004d420e1e
Keeeparis <vladimir1trotsenko@gmail.com>
2022-03-14 18:40:45 +0600

    update

--- .github/deploy.sh
+++ .github/deploy.sh
@@ -4,8 +4,6 @@ set -e
 
 if [ -d "/dist" ]
 then 
-    echo "Folder dist exists"
-    ls
     rm dist -r
 else
     echo "folder dist does not exist"
@@ -24,6 +22,6 @@ git config --local user.name "Keeeparis"
 
 git commit -m 'deploy'
 
-git push -f git@github.com:keeeparis/portfolio-2.git main:gh-pages
+# git push -f git@github.com:keeeparis/portfolio-2.git main:gh-pages
 
 cd -
