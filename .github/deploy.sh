dfe4468c92576725dbf92cbcecd8dcbcf7029c03
Keeeparis <vladimir1trotsenko@gmail.com>
2022-03-14 18:24:12 +0600

    add --local email and name

--- .github/deploy.sh
+++ .github/deploy.sh
@@ -18,6 +18,10 @@ cd dist
 git init 
 git checkout -b main
 git add -A
+
+git config --local user.email "vladimir1trotsenko@gmail.com"
+git config --local user.name "Keeeparis"
+
 git commit -m 'deploy'
 
 git push -f git@github.com:keeeparis/portfolio-2.git main:gh-pages
